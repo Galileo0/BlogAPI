cb3a484bc3f6c3d2fa43c724035cd1d787d782ee
Ahmed Zakaria <ahmed.zakaria@thebes.edu.eg>
2022-01-13 13:29:45 +0200

    INIT

---
+++ BlogApi/Models/Post.cs
@@ -0,0 +1,11 @@
+namespace BlogApi.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+
+    }
+}
